2c7af8dd5b53a4ca0ac614036c8807f18881e19d
joseademar <joseademarti@gmail.com>
2024-08-01 18:28:54 -0300

    Primeiro commit, criação do projeto e implementação da API

---
+++ AluGames/src/main/kotlin/Jogo.kt
@@ -0,0 +1,13 @@
+import com.google.gson.annotations.SerializedName
+
+class Jogo(@SerializedName("title") val titulo: String, @SerializedName("thumb") val capa: String) {
+
+    val descricao = "";
+
+    override fun toString(): String {
+        return "Meu jogo:\n" +
+                "Título: $titulo' \n" +
+                "Capa: $capa\n" +
+                "Descrição: $descricao";
+    }
+}
